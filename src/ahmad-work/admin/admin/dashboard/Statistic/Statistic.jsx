92376c320c6f8818b26857f5578bb8a8ae46865f
Ahmad4433 <110842437+Ahmad4433@users.noreply.github.com>
2024-01-31 18:08:09 +0500

    toggle added

---
+++ src/ahmad-work/admin/admin/dashboard/Statistic/Statistic.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import TopLosers from '../General Statistic/TopLosers'
+import Total from '../General Statistic/Total'
+import TopGainer from '../General Statistic/TopGainer'
+import style from './statistc.module.css'
+import ToggleBar from './ToggleBar'
+const Statistic = () => {
+  return (
+    <div className={style.main} >
+        <ToggleBar/>
+        <div className={style.level} >
+          <Total/>
+          <TopGainer/>
+          <TopLosers/>
+        </div>
+
+    </div>
+  )
+}
+
+export default Statistic
