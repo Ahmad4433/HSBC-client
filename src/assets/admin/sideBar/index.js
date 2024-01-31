b7043f1fe1ad10da3fbb19c9b3919f34d8c4682b
wahab315 <abdulwahab315tts@gmail.com>
2024-01-31 17:19:19 +0500

    navbar Basic Layout Doen

--- src/assets/admin/sideBar/index.js
+++ src/assets/admin/sideBar/index.js
@@ -1,10 +1,10 @@
 export { default as chatTeardropDots } from "./chatTeardropDots.svg";
 export { default as clientProdpect } from "./client-prospect.svg";
-export { default as collaboration } from "./collaboration.svg";
+export { default as collaboration } from "./collabration.svg";
 export { default as collection } from "./collection.svg";
 export { default as company } from "./company.svg";
 export { default as contract } from "./contract.svg";
-export { default as current } from "./current.svg";
+export { default as current } from "./current-Converstation.svg";
 export { default as invitation } from "./invitation.svg";
 export { default as negosiation } from "./negosiation.svg";
 export { default as overYourself } from "./overYourself.svg";
@@ -12,4 +12,4 @@ export { default as paiment } from "./paiment.svg";
 export { default as personalization } from "./personalization.svg";
 export { default as product } from "./product.svg";
 export { default as transaction } from "./transaction.svg";
-export { default as dashboard } from "/dashboard.svg";
+export { default as dashboard } from "./dashboard.svg";
--- src/components/admin/common/Sidebar.jsx
+++ src/components/admin/common/Sidebar.jsx
@@ -1,19 +1,46 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { RiArrowRightSLine } from "react-icons/ri";
+import { logo } from "../../../assets/user/navbar";
+import {
+  sidebarMoreRoutes,
+  sidebarRoutes,
+} from "../../../data/routes/sidebarRoutes";
 
 const Sidebar = () => {
   return (
     <>
       <div className="sidebar">
         <div className="sidebar-container">
-          <div className="sidebar-container-mainMenus">
-            <div>
-              <div>icon</div>
-              <p>Dashboard</p>
-              <RiArrowRightSLine />
-            </div>
+          <div className="sidebar-container-logo">
+            <img src={logo} alt="logo" />
           </div>
-          <div className="sidebar-container-subMenus"></div>
+          <div className="sidebar-container-mainMenus">
+            {sidebarRoutes.map((item) => (
+              <>
+                <Link to={item.to}>
+                  <div>
+                    <img src={item.icon} alt="icon" />
+                    <p>{item.text}</p>
+                  </div>
+                  <RiArrowRightSLine />
+                </Link>
+              </>
+            ))}
+          </div>
+          <div className="sidebar-container-subMenus">
+            {sidebarMoreRoutes.map((item) => (
+              <>
+                <Link to={item.to}>
+                  <div>
+                    <img src={item.icon} alt="icon" />
+                    <p>{item.text}</p>
+                  </div>
+                  <RiArrowRightSLine />
+                </Link>
+              </>
+            ))}
+          </div>
           <div className="sidebar-container-profile"></div>
         </div>
       </div>
