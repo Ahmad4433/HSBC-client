9ade4d38716d8d9bfbf7e0f3aab5d16e57af088f
Ahmad4433 <aliraza126005#gmail.com>
2024-02-08 18:00:14 +0500

    admin logout button added

--- src/components/admin/common/Sidebar.jsx
+++ src/components/admin/common/Sidebar.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { RiArrowRightSLine } from "react-icons/ri";
 import { logo } from "../../../assets/user/navbar";
+import adminprofile from "../../../ahmad-work/assets/adminprofile.png";
+import adminprofilearrow from "../../../ahmad-work/assets/adminprofilearrow.png";
+import { CiLogout } from "react-icons/ci";
+import {useNavigate} from 'react-router-dom'
+
 import {
   sidebarMoreRoutes,
   sidebarRoutes,
 } from "../../../data/routes/sidebarRoutes";
 
 const Sidebar = () => {
+  const navigate = useNavigate()
+
+const [isLogout,setIsLogout] = useState(false)
+
+const clickHandler = ()=>{
+  setIsLogout(!isLogout)
+}
+
+
+const logoutHandler = ()=>{
+  localStorage.removeItem('userCode')
+  navigate('/')
+  
+}
+
+
   return (
     <>
       <div className="sidebar">
@@ -37,7 +58,24 @@ const Sidebar = () => {
               </Link>
             ))}
           </div>
-          <div className="sidebar-container-profile"></div>
+          <div className="sidebar-container-profile">
+            <div   className={` admin-profile-sec ${isLogout ? 'click':''}`} >
+              <div onClick={clickHandler} className="admin-profile">
+                <img
+                  src={adminprofile}
+                  alt="admin"
+                  className="admin-profile-image"
+                />
+                <p className="admin-name">Veronica Sisilia</p>
+                <img src={adminprofilearrow}  className="admin-profile-opener" />
+              </div>
+              <div onClick={logoutHandler} className="admin-profile-logout-section" >
+          
+                <p className="admin-logout-title" >logout</p>
+                <CiLogout className="admin-logout-icon" />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
