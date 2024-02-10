9e7e24f3b9570d64681303d51db1edb512e86d8e
Ahmad4433 <aliraza126005#gmail.com>
2024-02-10 21:01:38 +0500

    ds

--- src/ahmad-work/customer/login/login-alternative/Alternative.jsx
+++ src/ahmad-work/customer/login/login-alternative/Alternative.jsx
@@ -2,17 +2,23 @@ import React from 'react'
 import style from './alternative.module.css'
 import google1 from '../../../assets/google 1.png'
 import face1 from '../../../assets/facebook 1.png'
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+import GoogleIcon from '@mui/icons-material/Google';
+
+
 const Alternative = () => {
   return (
     <div className={style.main} >
       
       <div className={style.actions} >
         <div className={style.image1}  >
-        <img src={google1} />
+    <GoogleIcon color='secondary' fontSize='large' />
+     
         <p>Google</p>
         </div>
       <div className={style.image2}  >
-      <img src={face1} />
+  
+        <FacebookRoundedIcon fontSize='large' color='primary' />
       <p>Facebook</p>
       </div>
       </div>
